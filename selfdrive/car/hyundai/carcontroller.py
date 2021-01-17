ae96924bf64158948be776fd774ffe9d94abafbb
conan <conan9879@naver.com>
2021-01-17 12:40:18 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -239,8 +239,8 @@ class CarController():
       apply_steer = self.limit_ctrl( apply_steer, apply_steer_limit, 0 )
 
 
-    if self.model_speed < 150:
-      apply_steer = self.movAvg.get_avg(apply_steer, 5)
+    #if self.model_speed < 150:
+    #  apply_steer = self.movAvg.get_avg(apply_steer, 5)
 
     # disable if steer angle reach 90 deg, otherwise mdps fault in some models
     lkas_active = enabled and abs(CS.out.steeringAngle) < 90.
